0a5d19fe15615d2f7870731ccc51b1ec2e5af3c4
sebastian trujillo <sebastian.trujillo02@uceva.edu.co>
2025-01-25 16:23:52 -0500

    proyecto terminado

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let cadena = "";
+
+function sorteo() {
+  return Math.floor(Math.random() * amigos.length);
+}
+
+function agregarAmigo() {
+  let sorteo = Math.floor(Math.random() * amigos.length);
+
+  let nombre = document.getElementById("amigo").value;
+  if (nombre == "" || nombre == " ") {
+    alert("Debe ingresar un nombre");
+  } else {
+    amigos.push(nombre);
+
+    let elemento = document.getElementById("listaAmigos");
+    cadena = nombre + "<br>" + cadena;
+    elemento.innerHTML = cadena;
+    document.getElementById("amigo").value = "";
+    
+  }
+ 
+}
+
+function sortearAmigo() {
+  let mostrar = document.getElementById("resultado");
+  mostrar.innerHTML = `el ganador fue  ${amigos[sorteo()]}`;
+}
